f4b1225d90622489efe6cb85038663eedbc68325
SyedTalha71x <syedtalha71x@gmail.com>
2025-02-13 14:55:43 +0500

    update

--- client/src/components/modal.jsx
+++ client/src/components/modal.jsx
@@ -9,7 +9,7 @@ const Modal = ({ isVisible, onClose, children }) => {
       <div className="fixed inset-0 flex items-center justify-center z-[100]">
         <div className="fixed inset-0 bg-black/70 backdrop-blur-sm" onClick={onClose}></div>
   
-        <div className="relative bg-white dark:bg-black rounded-xl shadow-2xl z-[101] p-6 w-[90vw] h-[85vh] max-w-6xl flex flex-col">
+        <div className="relative  bg-black rounded-xl shadow-2xl z-[101] p-6 w-[90vw] h-[85vh] max-w-6xl flex flex-col">
           <button
             className="sticky top-2 left-[calc(100%-2.5rem)] z-[102] w-8 h-8 flex items-center justify-center rounded-lg bg-white dark:bg-gray-800 text-gray-500 hover:text-gray-700 hover:bg-gray-100 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-200 transition-colors text-2xl"
             onClick={onClose}
