50640a5745af53e67237e37625d1f688d373cf10
Umair Khan <mrumairkhan74@gmail.com>
2026-02-02 18:33:39 +0500

    Minor issue solved

--- server/controllers/MemberController.js
+++ server/controllers/MemberController.js
@@ -151,18 +151,18 @@ const loginMember = async (req, res, next) => {
 
     res.cookie("token", AccessToken, {
       httpOnly: true,
-      //secure: process.env.NODE_ENV === "production", // true if on https
-      sameSite: "lax", 
-      //sameSite: "None",
+      secure: process.env.NODE_ENV === "production", // true if on https
+      //sameSite: "lax", 
+      sameSite: "None",
 
       maxAge: 24 * 60 * 1000, // 15 minutes (or whatever your access token expiry is)
     });
 
     res.cookie("refreshToken", RefreshToken, {
       httpOnly: true,
-      //secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
-      //sameSite: "None",
+      secure: process.env.NODE_ENV === "production",
+      //sameSite: "lax",
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
