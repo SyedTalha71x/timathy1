d3414a3ec2da2a2119ca624d1781316c62cc372e
orgagym <orgagym.marketing@gmail.com>
2026-03-09 19:38:41 +0100

    Layout Fix Configuration Contract Settings

--- client/src/components/studio-components/staff-components/edit-staff-modal.jsx
+++ client/src/components/studio-components/staff-components/edit-staff-modal.jsx
@@ -287,7 +287,7 @@ function EditStaffModal({
 
         {/* Form Content */}
         <form onSubmit={handleSubmit} className="flex flex-col flex-1 min-h-0">
-          <div className="flex-1 overflow-y-auto custom-scrollbar space-y-4 pr-1">
+         <div className="flex-1 overflow-y-auto overflow-x-hidden custom-scrollbar space-y-4 pr-1">
             {activeTab === "details" && (
               <>
                 {/* Avatar Upload */}
