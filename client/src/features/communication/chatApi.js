87c4f26c8a8a7fc831431bf82127290cef561123
Umair Khan <mrumairkhan74@gmail.com>
2026-03-25 14:37:28 +0500

    member communication is successfully completed there is ui issue kindly adjust it

--- client/src/features/communication/chatApi.js
+++ client/src/features/communication/chatApi.js
@@ -2,14 +2,14 @@ import api from '../../services/apiClient'
 
 
 // access chat or create chat 
-const accessChat = async (userData) => {
+export const accessChat = async (userData) => {
     const res = await api.post('/chat/access-chat', userData, { withCredentials: true })
     return res.data
 }
 
 // create Group
 
-const createGroup = async (groupData) => {
+export const createGroup = async (groupData) => {
     const res = await api.post('/chat/create-group', groupData, { withCredentials: true })
     return res.data
 }
@@ -17,14 +17,30 @@ const createGroup = async (groupData) => {
 
 // send message 
 
-const sendMessage = async (messageData) => {
+export const sendMessage = async (messageData) => {
     const res = await api.post('/chat/send-message', messageData, { withCredentials: true })
     return res.data
 }
 
 
 // fetch message 
-const allMessage = async (chatId) => {
+export const allMessage = async (chatId) => {
     const res = await api.get(`/chat/message/${chatId}`, { withCredentials: true })
     return res.data
+}
+
+
+
+// access Studio Chat 
+
+export const accessStudioChat = async (chatData) => {
+    const res = await api.post('/chat/studio/access', chatData, { withCredentials: true })
+    return res.data;
+}
+
+// fetch all studio chat
+
+export const fetchStudioChat = async () => {
+    const res = await api.get('/chat/studio/chats', { withCredentials: true })
+    return res.data;
 }
--- server/models/communicationModel/chatModel.js
+++ server/models/communicationModel/chatModel.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 
-
 const chatSchema = new mongoose.Schema({
     isGroupChat: {
         type: Boolean,
@@ -9,12 +8,33 @@ const chatSchema = new mongoose.Schema({
     chatName: {
         type: String
     },
+
+    // For user-to-user OR group chat
     users: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User'
-    }]
+    }],
+
+    // For studio chat
+    member: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    studio: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Studio'
+    },
+
+    // Optional (very useful)
+    lastMessage: String,
+    assignedTo: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+
 }, { timestamps: true })
 
+chatSchema.index({ member: 1, studio: 1 }, { unique: true })
 
 const chatModel = mongoose.model('chats', chatSchema)
 module.exports = chatModel
